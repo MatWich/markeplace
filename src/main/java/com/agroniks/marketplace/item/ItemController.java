66231eedd1ab9c40fcb4ce418fd0b9dc1ddec8dd
MatWich <wicherskimateusz@gmail.com>
2023-05-27 17:54:17 +0200

    AGR0003- First Get Request in ItemController

---
+++ src/main/java/com/agroniks/marketplace/item/ItemController.java
@@ -0,0 +1,22 @@
+package com.agroniks.marketplace.item;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Item>> findAllItemsInMarketPlace() {
+        return ResponseEntity.of(itemService.findAll());
+    }
+}
