4265c10414452bfe40f3eae9e75e76fbdc39d009
MatWich <wicherskimateusz@gmail.com>
2023-05-27 20:43:21 +0200

    AGR0008- ArchUnit tests setup

--- src/main/java/com/agroniks/marketplace/item/jpa/ItemRepository.java
+++ src/main/java/com/agroniks/marketplace/item/jpa/ItemRepository.java
@@ -1,7 +1,10 @@
 package com.agroniks.marketplace.item.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
+@Repository
 public interface ItemRepository extends JpaRepository<ItemEntity, Integer> {
     String findByName(String name);
 }
---
+++ src/test/java/com/agroniks/marketplace/ItemPersistenceArchTest.java
@@ -0,0 +1,51 @@
+package com.agroniks.marketplace;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+
+@AnalyzeClasses(packages = "com.agroniks.marketplace.item.jpa", importOptions = {ImportOption.DoNotIncludeTests.class})
+public class ItemPersistenceArchTest {
+
+
+
+    @ArchTest
+    public void setFieldsInEntityShouldNotBePublic(JavaClasses jc) {
+        fields().that().areDeclaredInClassesThat().areAnnotatedWith(Entity.class).should().bePrivate().check(jc);
+    }
+
+    @ArchTest
+    public void setEntitiesShouldBePublic(JavaClasses jc) {
+        classes().that()
+                .areAnnotatedWith(Entity.class)
+                .should().bePublic()
+                .andShould().beAnnotatedWith(Table.class)
+                .because("I said so").check(jc);
+    }
+
+    @ArchTest
+    public void setServicesShouldOnlyAccessToRepositories(JavaClasses jc) {
+        classes().that().areAnnotatedWith(Service.class)
+                .should().accessClassesThat().areAnnotatedWith(Repository.class).check(jc);
+    }
+
+    @ArchTest
+    public void setControllerShouldPerformActionsViaServicesNotRepositories(JavaClasses jc) {
+        classes().that().areAnnotatedWith(Controller.class).or().areAnnotatedWith(RestController.class)
+                .should().accessClassesThat().areAnnotatedWith(Service.class)
+                .check(jc);
+    }
+
+}
+
