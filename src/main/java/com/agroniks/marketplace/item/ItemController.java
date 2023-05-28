1d6c069d9f985ac6f44dd843e0d9931c9bfdf55d
MatWich <wicherskimateusz@gmail.com>
2023-05-28 17:54:30 +0200

    AGR0013- First search for business logic

--- src/main/java/com/agroniks/marketplace/item/ItemController.java
+++ src/main/java/com/agroniks/marketplace/item/ItemController.java
@@ -1,26 +1,53 @@
 package com.agroniks.marketplace.item;
 
+import com.agroniks.marketplace.item.jpa.ItemEntity;
+import com.agroniks.marketplace.item.jpa.ItemEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /*
-* This class should only perform business logic and do not perform any db connection
-*/
+ * This class should only perform business logic and do not perform any db connection
+ */
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/item")
 public class ItemController {
 
     @Autowired
     private ItemService itemService;
 
-    @GetMapping("")
-    public ResponseEntity<List<Item>> findAllItemsInMarketPlace() {
-        return ResponseEntity.of(itemService.findAll());
+    @Autowired
+    private ItemEntityService itemEntityService;
+
+    @GetMapping("below/{price}")
+    public ResponseEntity<List<Item>> getAllItemsBelowGivenPrice(@PathVariable double price) {
+        List<ItemEntity> itemsEntitiesBelowGivenPrice = itemEntityService.findAll().get()
+                .stream()
+                .filter(itemEntity -> itemEntity.getPrice() <= price)
+                .sorted(Comparator.comparingDouble(ItemEntity::getPrice))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.of(Optional.ofNullable(itemService.convert(itemsEntitiesBelowGivenPrice)));
     }
+
+    @GetMapping("over/{price}")
+    public ResponseEntity<List<Item>> getAllItemsOverGivenPrice(@PathVariable double price) {
+        List<ItemEntity> itemsEntitiesBelowGivenPrice = itemEntityService.findAll().get()
+                .stream()
+                .filter(itemEntity -> itemEntity.getPrice() >= price)
+                .sorted(Comparator.comparingDouble(ItemEntity::getPrice))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.of(Optional.ofNullable(itemService.convert(itemsEntitiesBelowGivenPrice)));
+    }
+
 }
--- src/main/java/com/agroniks/marketplace/item/ItemService.java
+++ src/main/java/com/agroniks/marketplace/item/ItemService.java
@@ -16,28 +16,16 @@ import java.util.stream.Collectors;
 public class ItemService {
 
 
-    private static List<Item> items = new ArrayList<>();
-
-    static  {
-        items.add(new Item("Item1", "Item1", 1.1));
-        items.add(new Item("Item2", "Item2", 2.2));
-        items.add(new Item("Item3", "Item3", 3.3));
-    }
-
-    public Optional<List<Item>> findAll() {
-        return Optional.of(items);
-    }
-
     /*
     * Take one ItemEntity and converts it into Item
     * */
-    private Item convertEntityToRecord(ItemEntity itemEntity) {
+    public Item convert(ItemEntity itemEntity) {
         // for now, it will be sufficient
         return new Item(itemEntity.getName(), itemEntity.getDescription(), itemEntity.getPrice());
     }
 
-    private List<Item> convertEntityToRecord(List<ItemEntity> itemEntities) {
+    public List<Item> convert(List<ItemEntity> itemEntities) {
         return itemEntities.stream().map(
-                this::convertEntityToRecord).collect(Collectors.toList());
+                this::convert).collect(Collectors.toList());
     }
 }
