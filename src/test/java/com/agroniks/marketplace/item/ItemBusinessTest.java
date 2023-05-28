049439dff33215ab28b2a5a5cddcf20edd91b52f
MatWich <wicherskimateusz@gmail.com>
2023-05-28 18:54:18 +0200

    AGR0016- Small refactor

--- src/test/java/com/agroniks/marketplace/item/ItemBusinessTest.java
+++ src/test/java/com/agroniks/marketplace/item/ItemBusinessTest.java
@@ -24,26 +24,24 @@ public class ItemBusinessTest {
     @Autowired
     ItemService itemService;
 
-    private List<ItemEntity> items = new ArrayList<>();
-
 
     @Test
     void shouldReturnItemsOverPrice3() {
         // given:
-        items.add(new ItemEntity("Item1", "Item1", 1.1));
-        items.add(new ItemEntity("Item2", "Item2", 2.2));
-        items.add(new ItemEntity("Item3", "Item3", 3.3));
-
+        List<ItemEntity> items = getData();
         // when:
         when(itemEntityService.findAll()).thenReturn(Optional.of(items));
 
         // expected:
         Item expected = new Item("Item3", "Item3", 3.3);
-
-
         assertEquals(expected, itemService.getAllItemsOverGivenPrice(3.0).get().get(0));
-
-
     }
 
+    private List<ItemEntity> getData() {
+        List<ItemEntity> items = new ArrayList<>();
+        items.add(new ItemEntity("Item1", "Item1", 1.1));
+        items.add(new ItemEntity("Item2", "Item2", 2.2));
+        items.add(new ItemEntity("Item3", "Item3", 3.3));
+        return items;
+    }
 }
